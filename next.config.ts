6d0ae543eec79417a581b6ff6b0fab24f51ad4cf
Drishti160803 <ranadrishti04@gmail.com>
2026-06-11 16:12:08 +0530

    Remove static export for Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Tells Next.js to compile into static HTML/CSS/JS
   images: {
-    unoptimized: true, // Required for static export to handle Unsplash images
+    unoptimized: true, 
     remotePatterns: [
       { 
         protocol: "https", 
-        hostname: "images.unsplash.com" 
+        hostname: "://unsplash.com" 
       },
     ],
   },
 };
 
 export default nextConfig;
-
